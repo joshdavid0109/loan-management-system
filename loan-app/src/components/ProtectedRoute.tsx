68d821623539b9889ad7634c26fb5ae7bed9e4d0
Joshua <joshdavid0109@gmail.com>
2026-01-09 11:35:50 +0800

    - for deployment

--- loan-app/src/components/ProtectedRoute.tsx
+++ loan-app/src/components/ProtectedRoute.tsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import type { JSX } from "react";
 
 const ProtectedRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
   const { user, loading } = useAuth();
