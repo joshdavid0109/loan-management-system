7f7b29157c4b783a4605adda35342abfba449c6a
Joshua <joshdavid0109@gmail.com>
2026-01-25 10:42:11 +0800

    - UPDATE STRUCTURE

--- loan-app/vite.config.ts
+++ loan-app/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: ".",                 // 👈 index.html lives here
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
   plugins: [react()],
   css: {
     postcss: './postcss.config.cjs',
